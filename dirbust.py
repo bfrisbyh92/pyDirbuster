ac6d043da0ce02b8047d37f42d4c5a6a2cab65d8
bfrisbyh92 <bfrisbyh92@gmail.com>
2023-04-03 21:58:37 -0400

    .

--- dirbust.py
+++ dirbust.py
@@ -22,32 +22,41 @@ def main():
         wordlist = f.read().splitlines()
 
     # Send requests
-    endpoints = []
-    for word in wordlist:
+    possible_endpoints = []
+    real_endpoints = []
+    for i, word in enumerate(wordlist):
         url = args.url + '/' + word
         response = requests.get(url)
         if response.status_code in [200, 201, 202, 203, 204, 205, 206, 207, 208, 226]:
-            endpoints.append(url)
-            print(Fore.GREEN +
-                  '[+] Possible 200 Status endpoint found: ' + url)
+            real_endpoints.append(url)
+            print(
+                Fore.GREEN + '[+] Possible 200 Status endpoint found ({0}): {1}'.format(i+1, url))
         elif response.status_code in [301, 302, 303, 304, 307, 308]:
-            endpoints.append(url)
-            print(Fore.YELLOW +
-                  '[+] Possible 300 Status endpoint found: ' + url)
+            possible_endpoints.append(url)
+            print(
+                Fore.YELLOW + '[~] Possible 300 Status endpoint found ({0}): {1}'.format(i+1, url))
         elif response.status_code in [401, 403]:
-            print(Fore.BLUE + '[+] Possible 400 Status endpoint found: ' + url)
+            possible_endpoints.append(url)
+            print(
+                Fore.BLUE + '[~] Possible 400 Status endpoint found ({0}): {1}'.format(i+1, url))
         elif response.status_code in [400, 404, 405, 406, 407, 408, 409, 410, 411, 412, 413, 414, 415, 416, 417, 418, 421, 422, 423, 424, 426, 428, 429, 431, 451]:
             if args.verbose:
                 print(
-                    Fore.RED + '[-] 500 Status Non-existent endpoint: ' + url)
+                    Fore.RED + '[-] 500 Status Non-existent endpoint ({0}): {1}'.format(i+1, url))
         elif response.status_code in [500, 501, 502, 503, 504, 505, 506, 507, 508, 510, 511]:
             if args.verbose:
-                print(Fore.RED + '[-] Possible server error: ' + url)
+                print(
+                    Fore.RED + '[-] Possible server error ({0}): {1}'.format(i+1, url))
 
-    # Write to output file
+    # Write results to output file
     if args.output:
         with open(args.output, 'w') as f:
-            f.write('\n'.join(endpoints))
+            f.write('[+] Real endpoints:\n')
+            f.write('\n'.join(real_endpoints))
+            f.write('\n\n[~] Possible endpoints:\n')
+            f.write('\n'.join(possible_endpoints))
+            print(Fore.YELLOW +
+                  '[*] Results written to output file: {0}'.format(args.output))
 
 
 if __name__ == '__main__':
